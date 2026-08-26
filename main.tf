f69495cd3064484d5df8fb68a639ae5519c849f1
shreya <shreya.mittal@ibm.com>
2026-08-26 20:43:42 +0530

    testing

--- main.tf
+++ main.tf
@@ -221,60 +221,60 @@ module "opensearch" {
   kms_key_arn              = module.kms.shared_key_arn
 }
 
-# module "elasticsearch" {
-#   source = "./modules/elasticsearch"
+module "elasticsearch" {
+  source = "./modules/elasticsearch"
 
-#   create_failing_resources = var.create_failing_resources
-#   tags                     = var.tags
-#   vpc_id                   = aws_vpc.main.id
-#   private_subnet_ids       = aws_subnet.private[*].id
-#   private_subnet_cidrs     = var.private_subnet_cidrs
-#   kms_key_arn              = module.kms.shared_key_arn
-# }
+  create_failing_resources = var.create_failing_resources
+  tags                     = var.tags
+  vpc_id                   = aws_vpc.main.id
+  private_subnet_ids       = aws_subnet.private[*].id
+  private_subnet_cidrs     = var.private_subnet_cidrs
+  kms_key_arn              = module.kms.shared_key_arn
+}
 
-# module "msk" {
-#   source = "./modules/msk"
+module "msk" {
+  source = "./modules/msk"
 
-#   create_failing_resources = var.create_failing_resources
-#   tags                     = var.tags
-#   vpc_id                   = aws_vpc.main.id
-#   private_subnet_ids       = aws_subnet.private[*].id
-#   private_subnet_cidrs     = var.private_subnet_cidrs
-#   kms_key_arn              = module.kms.shared_key_arn
-# }
+  create_failing_resources = var.create_failing_resources
+  tags                     = var.tags
+  vpc_id                   = aws_vpc.main.id
+  private_subnet_ids       = aws_subnet.private[*].id
+  private_subnet_cidrs     = var.private_subnet_cidrs
+  kms_key_arn              = module.kms.shared_key_arn
+}
 
-# module "neptune" {
-#   source = "./modules/neptune"
+module "neptune" {
+  source = "./modules/neptune"
 
-#   create_failing_resources = var.create_failing_resources
-#   tags                     = var.tags
-#   vpc_id                   = aws_vpc.main.id
-#   private_subnet_ids       = aws_subnet.private[*].id
-#   private_subnet_cidrs     = var.private_subnet_cidrs
-#   kms_key_arn              = module.kms.shared_key_arn
-# }
+  create_failing_resources = var.create_failing_resources
+  tags                     = var.tags
+  vpc_id                   = aws_vpc.main.id
+  private_subnet_ids       = aws_subnet.private[*].id
+  private_subnet_cidrs     = var.private_subnet_cidrs
+  kms_key_arn              = module.kms.shared_key_arn
+}
 
-# module "docdb" {
-#   source = "./modules/docdb"
+module "docdb" {
+  source = "./modules/docdb"
 
-#   create_failing_resources = var.create_failing_resources
-#   tags                     = var.tags
-#   vpc_id                   = aws_vpc.main.id
-#   private_subnet_ids       = aws_subnet.private[*].id
-#   private_subnet_cidrs     = var.private_subnet_cidrs
-#   kms_key_arn              = module.kms.shared_key_arn
-# }
+  create_failing_resources = var.create_failing_resources
+  tags                     = var.tags
+  vpc_id                   = aws_vpc.main.id
+  private_subnet_ids       = aws_subnet.private[*].id
+  private_subnet_cidrs     = var.private_subnet_cidrs
+  kms_key_arn              = module.kms.shared_key_arn
+}
 
-# module "elasticache" {
-#   source = "./modules/elasticache"
+module "elasticache" {
+  source = "./modules/elasticache"
 
-#   create_failing_resources = var.create_failing_resources
-#   tags                     = var.tags
-#   vpc_id                   = aws_vpc.main.id
-#   private_subnet_ids       = aws_subnet.private[*].id
-#   private_subnet_cidrs     = var.private_subnet_cidrs
-#   kms_key_arn              = module.kms.shared_key_arn
-# }
+  create_failing_resources = var.create_failing_resources
+  tags                     = var.tags
+  vpc_id                   = aws_vpc.main.id
+  private_subnet_ids       = aws_subnet.private[*].id
+  private_subnet_cidrs     = var.private_subnet_cidrs
+  kms_key_arn              = module.kms.shared_key_arn
+}
 
 # # ---------------------------------------------------------------------------
 # # Messaging + secrets
