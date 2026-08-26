c5054b2fa2f20b8cef6f366b8040931a4e10cba4
shreya <shreya.mittal@ibm.com>
2026-08-26 20:34:04 +0530

    testing

--- main.tf
+++ main.tf
@@ -254,19 +254,8 @@ module "neptune" {
   kms_key_arn              = module.kms.shared_key_arn
 }
 
-# module "docdb" {
-#   source = "./modules/docdb"
-
-#   create_failing_resources = var.create_failing_resources
-#   tags                     = var.tags
-#   vpc_id                   = aws_vpc.main.id
-#   private_subnet_ids       = aws_subnet.private[*].id
-#   private_subnet_cidrs     = var.private_subnet_cidrs
-#   kms_key_arn              = module.kms.shared_key_arn
-# }
-
-module "elasticache" {
-  source = "./modules/elasticache"
+module "docdb" {
+  source = "./modules/docdb"
 
   create_failing_resources = var.create_failing_resources
   tags                     = var.tags
@@ -275,6 +264,17 @@ module "elasticache" {
   private_subnet_cidrs     = var.private_subnet_cidrs
   kms_key_arn              = module.kms.shared_key_arn
 }
+
+# module "elasticache" {
+#   source = "./modules/elasticache"
+
+#   create_failing_resources = var.create_failing_resources
+#   tags                     = var.tags
+#   vpc_id                   = aws_vpc.main.id
+#   private_subnet_ids       = aws_subnet.private[*].id
+#   private_subnet_cidrs     = var.private_subnet_cidrs
+#   kms_key_arn              = module.kms.shared_key_arn
+# }
 
 # # ---------------------------------------------------------------------------
 # # Messaging + secrets
